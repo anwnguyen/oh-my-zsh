6d368fcdbd90cd1620dac89482bcc0695ff7785e
Adam Sumner <adam@crunchbase.com>
2013-07-10 14:13:03 -0700

    stop loading templates, add setup.sh for post-cloning setup

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# brew update
+# brew install git zsh
+
+[ ! -f /Users/Adam/.zshrc ]          && cp /Users/Adam/.oh-my-zsh/templates/zshrc.zsh-template          /Users/Adam/.zshrc
+[ ! -f /Users/Adam/.variables ]      && cp /Users/Adam/.oh-my-zsh/templates/variables.zsh-template      /Users/Adam/.variables
+[ ! -f /Users/Adam/.aliases ]        && cp /Users/Adam/.oh-my-zsh/templates/aliases.zsh-template        /Users/Adam/.aliases
+[ ! -f /Users/Adam/.secrets ]        && cp /Users/Adam/.oh-my-zsh/templates/secrets.zsh-template        /Users/Adam/.secrets
+[ ! -f /Users/Adam/.customizations ] && cp /Users/Adam/.oh-my-zsh/templates/customizations.zsh-template /Users/Adam/.customizations
+
+echo "/usr/local/bin/zsh" >> /etc/shells
+chsh -s /usr/local/bin/zsh Adam
