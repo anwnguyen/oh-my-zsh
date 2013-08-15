d535c9923dc859f947dca10b0a9ae1a31794068c
Adam Sumner <adamsumner@gmail.com>
2013-08-15 01:43:04 -0700

    added gemrc

--- setup.sh
+++ setup.sh
@@ -8,6 +8,7 @@
 [ ! -f /Users/Adam/.aliases ]        && cp /Users/Adam/.oh-my-zsh/templates/aliases.zsh-template        /Users/Adam/.aliases
 [ ! -f /Users/Adam/.secrets ]        && cp /Users/Adam/.oh-my-zsh/templates/secrets.zsh-template        /Users/Adam/.secrets
 [ ! -f /Users/Adam/.customizations ] && cp /Users/Adam/.oh-my-zsh/templates/customizations.zsh-template /Users/Adam/.customizations
+[ ! -f /Users/Adam/.gemrc] && cp /Users/Adam/.oh-my-zsh/templates/gemrc.zsh-template /Users/Adam/.gemrc
 
 echo "/usr/local/bin/zsh" >> /etc/shells
 chsh -s /usr/local/bin/zsh Adam
