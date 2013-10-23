401af3a6b2ae214070865cce49fd3885caffcb46
Adam Sumner <Adam@Adams-MacBook-Pro.local>
2013-10-22 19:52:56 -0700

    Fixes

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ brew install `cat homebrew_packages.txt`
 [ ! -f ~/.aliases ]        && cp ~/.oh-my-zsh/templates/aliases.zsh-template        ~/.aliases
 [ ! -f ~/.secrets ]        && cp ~/.oh-my-zsh/templates/secrets.zsh-template        ~/.secrets
 [ ! -f ~/.customizations ] && cp ~/.oh-my-zsh/templates/customizations.zsh-template ~/.customizations
-[ ! -f ~/.gemrc]           && cp ~/.oh-my-zsh/templates/gemrc.zsh-template          ~/.gemrc
+[ ! -f ~/.gemrc ]          && cp ~/.oh-my-zsh/templates/gemrc.zsh-template          ~/.gemrc
 
-echo "/usr/local/bin/zsh" >> /etc/shells  # TODO grep to ensure that this line isn't there before appending it
-chsh -s /usr/local/bin/zsh `whoami`
+sudo echo "/usr/local/bin/zsh" >> /etc/shells  # TODO grep to ensure that this line isn't there before appending it
+sudo chsh -s /usr/local/bin/zsh `whoami`
